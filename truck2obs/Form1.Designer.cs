4062a68a7b9f9f9a556c5876cd6fa004c85908ed
daafies <47672648+daafies@users.noreply.github.com>
2020-05-15 16:53:27 -0700

    initial checking

---
+++ truck2obs/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace truck2obs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.languageBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 226);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 124);
+            this.textBox1.TabIndex = 0;
+            // 
+            // languageBox1
+            // 
+            this.languageBox1.FormattingEnabled = true;
+            this.languageBox1.Location = new System.Drawing.Point(23, 180);
+            this.languageBox1.Name = "languageBox1";
+            this.languageBox1.Size = new System.Drawing.Size(121, 21);
+            this.languageBox1.TabIndex = 1;
+            this.languageBox1.SelectedIndexChanged += new System.EventHandler(this.languageBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Language";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(905, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Window capture this area in OBS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(762, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 130);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(169, 178);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(106, 23);
+            this.colorButton.TabIndex = 5;
+            this.colorButton.Text = "background color";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(-2, -23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(396, 145);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(376, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "..";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "debug info:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "foreground color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.languageBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Truck2OBS v0,1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox languageBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ truck2obs/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace truck2obs
+{
+
+    public partial class Form1 : Form
+    {
+        private string telemURI = "http://127.0.0.1:25555/api/ets2/telemetry";
+        //private string telemURI = "http://192.168.1.207";
+
+        System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+
+        // This is the method to run when the timer is raised.
+        private void TimerEventProcessor(Object myObject,
+                                                EventArgs myEventArgs)
+        {
+            //myTimer.Stop();
+            //myTimer.Enabled = true;
+            UpdateTelem();
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+
+            // Sets the timer interval to 3 seconds.
+            myTimer.Interval = 3000;
+            myTimer.Start();
+
+            languageBox1.Items.AddRange(new object[] { "English", "Deutsch" });
+
+            languageBox1.SelectedItem = Properties.Settings.Default["lingo"];
+
+            if (Properties.Settings.Default["backcolor"]!=null)
+            {
+                label3.BackColor = this.BackColor = (Color)Properties.Settings.Default["backcolor"];
+            }
+            if (Properties.Settings.Default["forecolor"] != null)
+            {
+                label3.ForeColor = (System.Drawing.Color)Properties.Settings.Default["forecolor"];
+            }
+        }
+
+        public void UpdateTelem()
+        {
+            string tmplabel = "";
+
+            try
+            {
+
+                
+                WebRequest request = WebRequest.Create(telemURI);
+
+                WebResponse response = request.GetResponse();
+
+                string bla = ((HttpWebResponse)response).StatusDescription;
+
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    // Open the stream using a StreamReader for easy access.
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    string responseFromServer = reader.ReadToEnd();
+                    // Display the content.
+                    Console.WriteLine(responseFromServer);
+
+                    dynamic stuff = JsonConvert.DeserializeObject(responseFromServer);
+
+                    string gamename = stuff.game.gameName;
+                    string connected = stuff.game.connected;
+                    string cargo = stuff.trailer.name;
+                    string startCity = stuff.job.sourceCity;
+                    string destCity = stuff.job.destinationCity;
+                    int distRemaining = stuff.navigation.estimatedDistance;
+
+                    if (startCity.Equals(""))
+                    {
+                        if (languageBox1.SelectedItem.Equals("English"))
+                        {
+                            tmplabel = textBox1.Text = "Between jobs";
+                        }
+                        else
+                        {
+                            tmplabel = textBox1.Text = "Kein Aktueller Job";
+                        }
+                    }
+                    else
+                    {
+                        if (gamename.Equals("ATS"))
+                        {
+                            if (languageBox1.SelectedItem.Equals("English"))
+                            {
+                                tmplabel = startCity + " -> " + destCity + ". " + Math.Round((distRemaining * 0.00062137), 1) + " miles remaining";
+                                textBox1.Text = "game: " + gamename + "\r\ncargo: " + cargo + "\r\nstartCity: " + startCity + "\r\nDestination: " + destCity + "\r\nremaining distance: " + Math.Round((distRemaining * 0.00062137), 1) + " miles";
+                            }
+                            else
+                            {
+                                tmplabel = startCity + " -> " + destCity + ". Noch " + Math.Round((distRemaining * 0.00062137), 1) + " miles";
+                                textBox1.Text = "game: " + gamename + "\r\ncargo: " + cargo + "\r\nstartCity: " + startCity + "\r\nDestination: " + destCity + "\r\nremaining distance: " + Math.Round((distRemaining * 0.00062137), 1) + " miles";
+                            }
+                        }
+                        else if (gamename.Equals("ETS2"))
+                        {
+                                if (languageBox1.SelectedItem.Equals("English"))
+                                {
+                                    tmplabel = startCity + " -> " + destCity + ". " + Math.Round((distRemaining * 0.001), 1) + " km remaining";
+                                    textBox1.Text = "game: " + gamename + "\r\ncargo: " + cargo + "\r\nstartCity: " + startCity + "\r\nDestination: " + destCity + "\r\nremaining distance: " + Math.Round((distRemaining * 0.001), 1) + " km";
+                                }
+                                else
+                                {
+                                    tmplabel = startCity + " -> " + destCity + ". Noch " + Math.Round((distRemaining * 0.001), 1) + " km";
+                                    textBox1.Text = "game: " + gamename + "\r\ncargo: " + cargo + "\r\nstartCity: " + startCity + "\r\nDestination: " + destCity + "\r\nremaining distance: " + Math.Round((distRemaining * 0.001), 1) + " km";
+                                }
+                        }
+
+
+
+                    }
+
+                }
+            }
+            catch
+            {
+                if (languageBox1.SelectedItem.Equals("English"))
+                {
+                    tmplabel = "Please make sure to start the ETS2Telemetry server";
+                }
+                else if (languageBox1.SelectedItem.Equals("Deutsch"))
+                {
+                    tmplabel = "Bitte starten Sie den ETS2Telemetry Server";
+                }
+                
+            }
+
+            label3.Text = tmplabel;
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter("etsatsroute.txt"))
+            {
+                outputFile.WriteLine(tmplabel);
+            }
+
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = true;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            MyDialog.Color = this.BackColor;
+
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default["backcolor"] = label3.BackColor = this.BackColor = MyDialog.Color;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = true;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            MyDialog.Color = label3.ForeColor;
+
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default["forecolor"] = label3.ForeColor = MyDialog.Color;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void languageBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["lingo"] = languageBox1.SelectedItem;
+            Properties.Settings.Default.Save();
+        }
+    }
+
+    
+}
+
+
+/*
+ *{"game":{"connected":false,"gameName":null,"paused":false,"time":"0001-01-01T00:00:00Z","timeScale":0.0,"nextRestStopTime":"0001-01-01T00:00:00Z","version":"0.0","telemetryPluginVersion":"0"},
+ * "truck":{"id":"","make":"","model":"","speed":0.0,"cruiseControlSpeed":0.0,"cruiseControlOn":false,"odometer":0.0,"gear":0,"displayedGear":0,"forwardGears":0,"reverseGears":0,"shifterType":"","engineRpm":0.0,"engineRpmMax":0.0,"fuel":0.0,"fuelCapacity":0.0,"fuelAverageConsumption":0.0,"fuelWarningFactor":0.0,"fuelWarningOn":false,"wearEngine":0.0,"wearTransmission":0.0,"wearCabin":0.0,"wearChassis":0.0,"wearWheels":0.0,"userSteer":0.0,"userThrottle":0.0,"userBrake":0.0,"userClutch":0.0,"gameSteer":0.0,"gameThrottle":0.0,"gameBrake":0.0,"gameClutch":0.0,"shifterSlot":0,"engineOn":false,"electricOn":false,"wipersOn":false,"retarderBrake":0,"retarderStepCount":0,"parkBrakeOn":false,"motorBrakeOn":false,"brakeTemperature":0.0,"adblue":0.0,"adblueCapacity":0.0,"adblueAverageConsumption":0.0,"adblueWarningOn":false,"airPressure":0.0,"airPressureWarningOn":false,"airPressureWarningValue":0.0,"airPressureEmergencyOn":false,"airPressureEmergencyValue":0.0,"oilTemperature":0.0,"oilPressure":0.0,"oilPressureWarningOn":false,"oilPressureWarningValue":0.0,"waterTemperature":0.0,"waterTemperatureWarningOn":false,"waterTemperatureWarningValue":0.0,"batteryVoltage":0.0,"batteryVoltageWarningOn":false,"batteryVoltageWarningValue":0.0,"lightsDashboardValue":0.0,"lightsDashboardOn":false,"blinkerLeftActive":false,"blinkerRightActive":false,"blinkerLeftOn":false,"blinkerRightOn":false,"lightsParkingOn":false,"lightsBeamLowOn":false,"lightsBeamHighOn":false,"lightsAuxFrontOn":false,"lightsAuxRoofOn":false,"lightsBeaconOn":false,"lightsBrakeOn":false,"lightsReverseOn":false,
+ * "placement":{"x":0.0,"y":0.0,"z":0.0,"heading":0.0,"pitch":0.0,"roll":0.0},"acceleration":{"x":0.0,"y":0.0,"z":0.0},"head":{"x":0.0,"y":0.0,"z":0.0},"cabin":{"x":0.0,"y":0.0,"z":0.0},"hook":{"x":0.0,"y":0.0,"z":0.0}},
+ * "trailer":{"attached":false,"id":"","name":"","mass":0.0,"wear":0.0,"placement":{"x":0.0,"y":0.0,"z":0.0,"heading":0.0,"pitch":0.0,"roll":0.0}},
+ * "job":{"income":0,"deadlineTime":"0001-01-01T00:00:00Z","remainingTime":"0001-01-01T00:00:00Z","sourceCity":"","sourceCompany":"","destinationCity":"","destinationCompany":""},
+ * "navigation":{"estimatedTime":"0001-01-01T00:00:00Z","estimatedDistance":0,"speedLimit":0}}
+}
+
+*/
